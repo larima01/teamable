0e3cb5609ef7057042d4fc6125092e96d442e31f
Aminu Lukmon Olamilekan <lukasaminu@gmail.com>
2023-12-18 15:56:45 +0100

    second commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function handleEditProfile(){
+    document.getElementById("edit-view").style.display = "block";
+    document.getElementById("main-container").style.display = "none";
+}
+
+function handleUpdateProfile(){
+    document.getElementById("main-container").style.display = "block";
+    document.getElementById("edit-view").style.display = "none";
+}
